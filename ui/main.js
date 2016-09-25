e7cad4e4e79cc0f195e0f429dd31f6807957eb89
Durai <durai.kasinathan@gmail.com>
2016-09-25 19:29:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,11 +6,27 @@ var button = document.getElementById("counter");
 button.onclick = function() {
     
     // Make a request to the counter endpoint
+    var request = new XMLHttpRequest();
     
     // Capture the response and store it in a variable
+    request.onreadystatechange = function() {
+        if(request.readyState == XMLHttpRequest.DONE) {
+            
+            // Take some action    
+            if(request.status == 200)
+            {  
+                var counter = request.responseText;
+                var span = document.getElementById("count");
+                span.innerHTML = counter.toString();
+            }
+        
+            //Not done yet    
+        }
     
-    // Render the variable in the current span
-    counter = counter + 1;
-    var span = document.getElementById("count");
-    span.innerHTML = counter.toString();
+    };
+    
+    //Make the request
+    request.open('GET','http://http://kdurai.imad.hasura-app.io/counter',true);
+    request.send(null);
+    
 };
