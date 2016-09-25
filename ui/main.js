9e4881d5ca8aca3655942c5daebb4022e3c15513
Durai <durai.kasinathan@gmail.com>
2016-09-25 22:41:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,3 +30,22 @@ button.onclick = function() {
     request.send(null);
     
 };
+
+// Submit name
+var nameInput = document.getElementById("name");
+var name = nameInput.value;
+var submit = document.getElementById("submit_btn");
+submit.onClick = function() {
+  // Make request to the server and send the name
+  
+  // Capture a list of names and render it as a list
+  var names = ['name1','name2','name3','name4','name5'];
+  var list = '';
+  for(var i=0; i<names.length; i++) 
+  {
+    list +='<li>'+names[i] + '</li>';
+  }
+  
+  var ul = document.getElementById('namelist');
+  ul.innerHTML = list;
+};
