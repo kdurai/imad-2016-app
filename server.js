d588e37a30c4917fa0bfb0feec329f1619dc2533
Durai <durai.kasinathan@gmail.com>
2016-09-25 19:00:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -82,6 +82,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
+app.get("/counter", function(req,res) {
+   counter = counter + 1;
+   res.send(counter.toString());
+});
+
 app.get('/:articleName',function(req,res) { 
     //articleName=article-one
     //articles[articleName] = {} content object for article one
@@ -99,12 +105,6 @@ app.get('/ui/madi.png', function (req, res) {
 
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
-
-var counter = 0;
-app.get("/counter", function(req,res) {
-   counter = counter + 1;
-   res.send(counter.toString());
 });
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
