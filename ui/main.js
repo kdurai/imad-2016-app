64629d2d64227f67d98c3720b42770c383f9b7be
Durai <durai.kasinathan@gmail.com>
2016-09-25 21:58:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,7 +26,7 @@ button.onclick = function() {
     };
     
     //Make the request
-    request.open('GET','http://http://kdurai.imad.hasura-app.io/counter',true);
+    request.open('GET','http://kdurai.imad.hasura-app.io/counter',true);
     request.send(null);
     
 };
