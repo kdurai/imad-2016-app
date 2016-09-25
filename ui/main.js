535ef25ad2cf7ed3986378f40d67382dee80e942
Durai <durai.kasinathan@gmail.com>
2016-09-25 18:43:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,5 +9,12 @@ element.innerHTML="New Value";
 var img=document.getElementById("img");
 
 img.onclick=function() {
-  img.style.marginLeft= '100px'  ;
+//  img.style.marginLeft= '100px'  ;
+  var interval = setInterval(moveRight,100);
 };
+
+var marginLeft = 0;
+function moveRight() {
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
