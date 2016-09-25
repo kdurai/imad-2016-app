039397ab5ba95fdedfac3355935ce90a296c8345
Durai <durai.kasinathan@gmail.com>
2016-09-25 19:15:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,20 +1,16 @@
-console.log('Loaded!');
+// Counter Code
 
-//Change the text of main-text element
+var counter = 0;
+var button = document.getElementById("counter");
 
-var element=document.getElementById("main-text");
-
-element.innerHTML="New Value";
-
-var img=document.getElementById("img");
-
-img.onclick=function() {
-//  img.style.marginLeft= '100px'  ;
-  var interval = setInterval(moveRight,50);
+button.onclick = function() {
+    
+    // Make a request to the counter endpoint
+    
+    // Capture the response and store it in a variable
+    
+    // Render the variable in the current span
+    counter = counter + 1;
+    var span = document.getElementById("count");
+    span.innerHTML = counter.toString();
 };
-
-var marginLeft = 0;
-function moveRight() {
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-}
